7535d07835f4558e37cb3795bfcedd44556ab3f5
sawid <zasawidza@gmail.com>
2020-08-11 21:17:49 +0700

    Add project files.

---
+++ CE_Y1_W1_Work/Work2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	float x, y;
+	printf("Enter the first length : ");
+	scanf_s("%f", &x);
+	printf("Enter the second length : ");
+	scanf_s("%f", &y);
+	printf("%f", sqrt(pow(x, 2) + pow(y, 2)) );
+}
